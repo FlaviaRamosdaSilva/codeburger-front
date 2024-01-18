93b3b64348e10714a0f834ff68c1896fb48a4588
FlaviaRamosdaSilva <didi.flavia@gmail.com>
2024-01-18 17:20:48 -0300

    tentando-resolver

--- src/services/api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-// conforme documentação do axios
-
-const apiCodeBurger = axios.create({
-  baseURL: 'http://localhost:3001/' // endereço do backend
-})
-
-export default apiCodeBurger
